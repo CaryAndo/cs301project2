aee01f57198376e0ab9235a1c8e3a737c100005b
caryando <caryando@gmail.com>
2014-11-14 00:24:20 -0800

    Add docstring, goodnight.

--- project2.py
+++ project2.py
@@ -1,15 +1,17 @@
 table = []
 
-def setup_table():
-	theFile = open('input.txt', 'r')
-	for enum, line in enumerate(theFile):
-		line = line.replace('\n', '')
-		tony = line.split(' ')
-		table.append([])
 
-		for i, number in enumerate(tony):
-			table[enum].append(float(number))
+def setup_table():
+    """ Set up the table that holds x/y values """
+    the_file = open('input.txt', 'r')
+    for enum, line in enumerate(the_file):
+        line = line.replace('\n', '')
+        tony = line.split(' ')
+        table.append([])
+
+        for i, number in enumerate(tony):
+            table[enum].append(float(number))
 
 if __name__ == '__main__':
-	setup_table()
-	#print "Hello World!"
+    setup_table()
+    #print "Hello World!"
